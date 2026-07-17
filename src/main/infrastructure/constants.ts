9afd78199461acbde1658be47bd9191f25a215b9
Mark Benson <41453195+thealternator89@users.noreply.github.com>
2026-07-18 11:59:23 +1200

    feat: use #readme anchor for Stitch link in attribution statement

--- src/main/infrastructure/constants.ts
+++ src/main/infrastructure/constants.ts
@@ -1,2 +1,2 @@
 export const ATTRIBUTION_STATEMENT =
-  '> Generated with Stitch<sup>[^](https://github.com/thealternator89/stitch/)</sup> and GitHub Copilot<sup>[^](https://github.com/features/copilot)</sup>.';
+  '> Generated with Stitch<sup>[^](https://github.com/thealternator89/stitch/#readme)</sup> and GitHub Copilot<sup>[^](https://github.com/features/copilot)</sup>.';
