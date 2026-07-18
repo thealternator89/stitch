edf47ae5cb78e5ae22017bff53529793c03587a8
Mark Benson <41453195+thealternator89@users.noreply.github.com>
2026-07-18 13:52:03 +1200

    refactor: make STITCH_ATTRIBUTION and COPILOT_ATTRIBUTION private to constants.ts

--- src/main/infrastructure/constants.ts
+++ src/main/infrastructure/constants.ts
@@ -1,7 +1,7 @@
-export const STITCH_ATTRIBUTION =
+const STITCH_ATTRIBUTION =
   'Stitch<sup>[^](https://github.com/thealternator89/stitch/#readme)</sup>';
 
-export const COPILOT_ATTRIBUTION =
+const COPILOT_ATTRIBUTION =
   'GitHub Copilot<sup>[^](https://github.com/features/copilot)</sup>';
 
 export const ATTRIBUTION_STATEMENT_GENERATED = `> Generated with ${STITCH_ATTRIBUTION} and ${COPILOT_ATTRIBUTION}.`;
